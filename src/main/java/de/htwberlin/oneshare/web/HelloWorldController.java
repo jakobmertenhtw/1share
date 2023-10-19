f3562af028ac48a3a738cdb4e4fdba54899b1c19
jakobmertenhtw <s0587527@htw-berlin.de>
2023-10-19 20:42:43 +0200

    test commit

--- src/main/java/de/htwberlin/oneshare/web/HelloWorldController.java
+++ src/main/java/de/htwberlin/oneshare/web/HelloWorldController.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController {
 
+    // Test commit
+
     @GetMapping(path = "/")
     public ModelAndView showHelloWorldPage() {
         return new ModelAndView("helloworld");
