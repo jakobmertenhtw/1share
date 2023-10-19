1b5a443727dc8c6dfccf56c919464fc88fb9f8d3
jakobmertenhtw <147500519+jakobmertenhtw@users.noreply.github.com>
2023-10-19 18:21:28 +0200

    first commit

---
+++ src/test/java/de/htwberlin/oneshare/OneshareApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.oneshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
